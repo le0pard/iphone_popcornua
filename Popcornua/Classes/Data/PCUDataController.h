8b64c790445b4c2fe7f80206ffffa65b78271541
Alexey <leopard.not.a@gmail.com>
2011-09-11 02:05:50 +0300

    fix icons and splash screen images

---
+++ Popcornua/Classes/Data/PCUDataController.h
@@ -0,0 +1,19 @@
+//
+//  PCUDataController.h
+//  Popcornua
+//
+//  Created by Alex on 9/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ASIHTTPRequest.h"
+#import "Afisha.h"
+#import "Cinema.h"
+#import "Movie.h"
+
+@interface PCUDataController : NSObject
+
+@property (nonatomic, retain) NSError *error;
+
+@end
---
+++ Popcornua/Classes/Data/PCUDataController.m
@@ -0,0 +1,25 @@
+//
+//  PCUDataController.m
+//  Popcornua
+//
+//  Created by Alex on 9/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PCUDataController.h"
+
+@implementation PCUDataController
+
+@synthesize error;
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
